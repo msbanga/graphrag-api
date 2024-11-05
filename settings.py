cbd41b106513a2ff2b33f859a279452d86ff6985
mandsi <mandsi@adobe.com>
2024-11-05 23:04:28 +0530

    support for embeddings deployment models

--- settings.py
+++ settings.py
@@ -1,17 +1,26 @@
 from pydantic_settings import BaseSettings
 import yaml
 
+
 class Settings(BaseSettings):
+    GRAPHRAG_OPENAI_API_VERSION: str
+    GRAPHRAG_API_BASE: str
     GRAPHRAG_LLM_MODEL: str
-    GRAPHRAG_EMBEDDING_MODEL: str
     GRAPHRAG_CLAIM_EXTRACTION_ENABLED: bool
     INPUT_DIR: str
     COMMUNITY_LEVEL: int
     GRAPHRAG_API_KEY: str
+    GRAPHRAG_DEPLOYMENT_NAME: str
+
+    GRAPHRAG_EMBEDDING_MODEL: str
+    GRAPHRAG_EMBEDDING_API_BASE: str
+    EMBEDDING_DEPLOYMENT_NAME: str
+    EMBEDDING_API_VERSION: str
+    EMBEDDING_API_KEY: str
 
     class Config:
         env_file = ".env"
-        
+
 
 def load_settings_from_yaml(yaml_file: str) -> Settings:
     with open(yaml_file, 'r', encoding='utf-8') as file:
